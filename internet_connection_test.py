c864acac84b9fb149c7e9d6b5b3ffb0c3b9fe83a
H. Kamran <hkamran80@users.noreply.github.com>
2018-03-08 16:12:08 -0800

    Update internet_connection_test.py

--- internet_connection_test.py
+++ internet_connection_test.py
@@ -5,16 +5,16 @@ import time
 import sys
 
 def check(host="8.8.8.8", port=53, timeout=3):
-    # 8.8.8.8 is one of Google's DNS servers
-    # 4.4.4.4 is the other DNS server
+    # 8.8.8.8 is the primary Google DNS server
+    # 4.4.4.4 is the secondary server
     try:
         socket.setdefaulttimeout(timeout)
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
-        print "Connected"
+        print("Connected")
         sys.exit()
     except Exception as ex:
-        print ex.message
-        print "Not Connected"
+        print(ex.message)
+        print("Not Connected")
         os.system("sudo reboot")
         sys.exit()
 
